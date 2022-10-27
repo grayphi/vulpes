58fa83913538dd9901bc3f1e71d8dc497672e760
dineshsaini <dinesh.saini.277@gmail.com>
2022-10-27 22:54:18 +0530

    update

--- lib/core.rb
+++ lib/core.rb
@@ -3,10 +3,12 @@ require_relative 'vulpes/loader'
 require 'vulpes/object'
 require 'vulpes/constants'
 require 'defaults'
+
+Vulpes::Constants.add('VULPES_BASE', Vulpes::Defaults::Core.vulpes_base)
+
 require 'vulpes/errors'
 require 'vulpes/config'
 require 'vulpes/logger'
 require 'vulpes/module'
 require 'vulpes/dork'
-
-Vulpes::Constants.add('VULPES_BASE', Vulpes::Defaults::Core.vulpes_base)
+require 'db/cachemanager'
