96fd33f15a7ab28e9c86067f281a1c5b8b581422
dineshsaini <dinesh.saini.277@gmail.com>
2022-12-21 03:21:48 +0530

    update

--- lib/web/utils/urlutils.rb
+++ lib/web/utils/urlutils.rb
@@ -5,8 +5,13 @@ module Web
             super("URLUtils")
          end
 
-         
-         
+         def self.wait_for
+            n1 = Vulpes::Constants.get 'min_delay'
+            n2 = Vulpes::Constants.get 'max_delay'
+
+            n1 + ((n2 - n1) * Random.rand)
+         end
+
 
          private_class_method :new
       end
