a978ac0275a33a493ca8649865e2422d07f88e78
dineshsaini <dinesh.saini.277@gmail.com>
2023-03-14 23:23:12 +0530

    updates charset value for whole db

--- scripts/db/install/mysql.sql
+++ scripts/db/install/mysql.sql
@@ -2,7 +2,7 @@ ALTER USER IF EXISTS '$db_usr'@'$mysql_host' IDENTIFIED BY '$db_pass';
 
 CREATE USER IF NOT EXISTS '$db_usr'@'$mysql_host' IDENTIFIED BY '$db_pass';
 
-CREATE DATABASE $db_name CHARACTER SET utf8 COLLATE utf8_bin;
+CREATE DATABASE $db_name CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 GRANT ALL ON $db_name.* TO '$db_usr'@'$mysql_host'  WITH GRANT OPTION;
 
